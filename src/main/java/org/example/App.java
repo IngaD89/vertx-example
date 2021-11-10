a6c89c203fcce351029de3cb148398aa3ccbf0d0
Inga Demetrashvili <inga@MICROBR.local>
2021-11-10 13:31:38 +0100

    execute verticle on port 3000

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,40 +1,24 @@
 package org.example;
 
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
+import org.example.verticles.VerticalToDeploy;
 
 
 public class App {
     public static void main(String[] args) {
 
         Vertx vertx = Vertx.vertx();
-
-        HttpServer httpServer = vertx.createHttpServer();
-
-        Router router = Router.router(vertx);
-
-        Route handler1 = router
-                .get("/hello")
-                .handler(routingContext -> {
-                    HttpServerResponse response = routingContext.response();
-                    response.setChunked(true);
-                    response.write("Hello from vertx\n");
-                    response.end("ended");
-                });
-        Route handler2 = router
-                .post("/hello")
-                .handler(routingContext -> {
-                    HttpServerResponse response = routingContext.response();
-                    response.setChunked(true);
-                    response.write("Hello from vertx\n");
-                    response.end("posted");
-                });
-
-        httpServer
-                .requestHandler(router)
-                .listen(8080);
-    }
+        vertx.deployVerticle(new VerticalToDeploy(),
+                new DeploymentOptions()
+                        .setConfig(new JsonObject()
+                                .put("port", 3000)
+                        )
+        );
+}
 }
---
+++ src/main/java/org/example/verticles/VerticalToDeploy.java
@@ -0,0 +1,36 @@
+package org.example.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+
+public class VerticalToDeploy extends AbstractVerticle {
+
+    public void start() {
+        Router router = Router.router(vertx);
+        HttpServer server = vertx.createHttpServer();
+
+        Route handler1 = router
+                .get("/hello")
+                .handler(routingContext -> {
+                    HttpServerResponse response = routingContext.response();
+                    response.setChunked(true);
+                    response.write("Hello from vertx\n");
+                    response.end("ended");
+                });
+        Route handler2 = router
+                .post("/hello")
+                .handler(routingContext -> {
+                    HttpServerResponse response = routingContext.response();
+                    response.setChunked(true);
+                    response.write("Hello from vertx\n");
+                    response.end("posted");
+                });
+
+        server.requestHandler(router).listen(config().getInteger("port"));
+    }
+}
+
